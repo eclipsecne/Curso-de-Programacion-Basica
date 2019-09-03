7020f97165184d08983929506352ac902cf23656
Eclipse <eclipsecnelalo93@gmail.com>
2019-09-02 21:25:56 -0500

    Nuevos apartados

---
+++ 12 Dibujando en el DOM/dibujo.js
@@ -0,0 +1,4 @@
+//Obtencion del elemento dbujito por el ID
+var d = document.getElementById("dibujito");
+//Funcion del objeto canvas que me permite obtener el area donde voy a dibujar
+var lienzo = d.getContext("2d");
